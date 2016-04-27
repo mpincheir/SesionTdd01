efaf6d31d7216d268ff37017ed480e585e676376
Miguel Pincheira <mpincheir@ubiobio.cl>
2016-04-27 11:43:11 -0300

    Primer Test

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -1,5 +1,20 @@
 package cl.ubb.agil;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class CalculadoraTest {
+
+	@Test
+	public void DosMasTresEsCinco(){
+	
+		Calculadora calculadora = new Calculadora();
+		
+		int resultado = calculadora.sumar(2,3);
+		
+		assertThat(resultado,is(5));
+	}
 
 }
